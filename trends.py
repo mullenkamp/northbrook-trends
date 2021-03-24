08a58097119a7fb842cdf2d25694140f074c9d1c
mike <mullenkamp1@gmail.com>
2021-03-24 21:05:20 +1300

    update

--- trends.py
+++ trends.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 from fbprophet import Prophet, plot
 import io
-import pickle
 import yaml
 from tethysts import Tethys
 import xarray as xr
@@ -36,6 +35,8 @@ with open(os.path.join(base_dir, 'parameters.yml')) as param:
     param = yaml.safe_load(param)
 
 
+mcmc_samples = 0 # Change to 300 to add uncertainty analysis to the outputs; adds a lot more processing time.
+
 owner = 'Environment Canterbury'
 product_code = 'quality_controlled_data'
 
@@ -45,7 +46,7 @@ gwl_rec_ref = ['M35/2679', 'M35/0366']
 # precip_ref = ['322410', '322211', '322110', '321212', '321310']
 precip_ref = ['322110', '321310']
 
-s1 = '1500fc71aab1fb7d0d4786a0'
+# s1 = '1500fc71aab1fb7d0d4786a0'
 
 groups = {1: ['M35/2679', 'M35/0366', 'M35/0472', 'M35/0538'], 2: ['M35/0312', 'M35/0222'], 3: ['M34/0232', 'M34/0207'], 4: ['66204', '66213', '66210'], 5: ['66417'], 6: ['66214'], 7: ['322110', '321310']}
 
@@ -58,6 +59,144 @@ x_axis_labels = {'yearly_trend': 'Date', 'seasonality': 'Day of year', 'ts_plot'
 flow_stns_shp = 'flow_sites_2021-03-16.shp'
 gwl_stns_shp = 'gwl_sites_2021-03-16.shp'
 precip_stns_shp = 'precip_sites_2021-03-16.shp'
+
+###################################
+### Extra functions
+
+
+def stns_dict_to_gdf(stns):
+    """
+
+    """
+    stns1 = copy.deepcopy(stns)
+    geo1 = [shapely.geometry.Point(s['geometry']['coordinates']) for s in stns1]
+
+    [s.update({'min': s['stats']['min'], 'max': s['stats']['max'], 'from_date': s['stats']['from_date'], 'to_date': s['stats']['to_date']}) for s in stns1]
+    [(s.pop('stats'), s.pop('geometry'), s.pop('results_object_key'), s.pop('virtual_station')) for s in stns1]
+    [s.pop('properties') for s in stns1 if 'properties' in s]
+
+    df1 = pd.DataFrame(stns1)
+
+    stns_gpd1 = gpd.GeoDataFrame(df1, crs=4326, geometry=geo1)
+
+    return stns_gpd1
+
+
+def set_y_as_percent(ax):
+    yticks = 100 * ax.get_yticks()
+    yticklabels = ['{0:.4g}%'.format(y) for y in yticks]
+    ax.set_yticklabels(yticklabels)
+    return ax
+
+
+def plot_trends(m, fcst, x_label, y_label, legend_label, color='#0072B2', ax=None, uncertainty=True, plot_cap=False, figsize=(15, 10)):
+    """
+    Plot a particular component of the forecast.
+
+    Parameters
+    ----------
+    m: Prophet model.
+    fcst: pd.DataFrame output of m.predict.
+    ax: Optional matplotlib Axes to plot on.
+    uncertainty: Optional boolean to plot uncertainty intervals, which will
+        only be done if m.uncertainty_samples > 0.
+    plot_cap: Optional boolean indicating if the capacity should be shown
+        in the figure, if available.
+    figsize: Optional tuple width, height in inches.
+
+    Returns
+    -------
+    matplotlib ax
+    """
+    set_style("white")
+    set_style("ticks")
+    # set_context('poster')
+    set_context('talk')
+
+    artists = []
+    if not ax:
+        fig = plt.figure(facecolor='w', figsize=figsize)
+        ax = fig.add_subplot(111)
+    fcst_t = fcst['ds'].dt.to_pydatetime()
+    artists += ax.plot(fcst_t, fcst['trend'], ls='-', c=color, label=legend_label)
+    if 'cap' in fcst and plot_cap:
+        artists += ax.plot(fcst_t, fcst['cap'], ls='--', c='k')
+    if m.logistic_floor and 'floor' in fcst and plot_cap:
+        ax.plot(fcst_t, fcst['floor'], ls='--', c='k')
+    if uncertainty and m.uncertainty_samples:
+        artists += [ax.fill_between(
+            fcst_t, fcst['trend_lower'], fcst['trend_upper'],
+            color=color, alpha=0.2)]
+    # Specify formatting to workaround matplotlib issue #12925
+    locator = AutoDateLocator(interval_multiples=False)
+    formatter = AutoDateFormatter(locator)
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+    ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    if 'trend' in m.component_modes['multiplicative']:
+        ax = set_y_as_percent(ax)
+    plt.tight_layout()
+    despine()
+    return ax
+
+
+def plot_seasonality(m, fcst, x_label, y_label, legend_label, color='#0072B2', ax=None, uncertainty=True, plot_cap=False, figsize=(15, 10)):
+    """
+    Plot a particular component of the forecast.
+
+    Parameters
+    ----------
+    m: Prophet model.
+    fcst: pd.DataFrame output of m.predict.
+    ax: Optional matplotlib Axes to plot on.
+    uncertainty: Optional boolean to plot uncertainty intervals, which will
+        only be done if m.uncertainty_samples > 0.
+    plot_cap: Optional boolean indicating if the capacity should be shown
+        in the figure, if available.
+    figsize: Optional tuple width, height in inches.
+
+    Returns
+    -------
+    matplotlib ax
+    """
+    set_style("white")
+    set_style("ticks")
+    # set_context('poster')
+    set_context('talk')
+
+    artists = []
+    if not ax:
+        fig = plt.figure(facecolor='w', figsize=figsize)
+        ax = fig.add_subplot(111)
+    ## Prepare data
+    fcst1 = fcst.groupby(fcst['ds'].dt.dayofyear).mean()
+
+    fcst_t = fcst1.index
+    artists += ax.plot(fcst_t, fcst1['yearly'], ls='-', c=color, label=legend_label)
+    if 'cap' in fcst1 and plot_cap:
+        artists += ax.plot(fcst_t, fcst1['cap'], ls='--', c='k')
+    if m.logistic_floor and 'floor' in fcst1 and plot_cap:
+        ax.plot(fcst_t, fcst1['floor'], ls='--', c='k')
+    if uncertainty and m.uncertainty_samples:
+        artists += [ax.fill_between(
+            fcst_t, fcst1['yearly_lower'], fcst1['yearly_upper'],
+            color=color, alpha=0.2)]
+    # Specify formatting to workaround matplotlib issue #12925
+    # locator = AutoDateLocator(interval_multiples=False)
+    # formatter = AutoDateFormatter(locator)
+    # ax.xaxis.set_major_locator(locator)
+    # ax.xaxis.set_major_formatter(formatter)
+    ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    if 'trend' in m.component_modes['multiplicative']:
+        ax = set_y_as_percent(ax)
+    plt.tight_layout()
+    despine()
+    return ax
+
 
 ##################################
 ### Get data
@@ -92,6 +231,7 @@ precip_stns = [s for s in precip_stns0 if s['ref'] in precip_ref]
 flow_stn_ids = [s['station_id'] for s in flow_stns]
 flow_dict = tethys1.get_bulk_results(flow_ds['dataset_id'], flow_stn_ids, remove_height=True)
 
+# Prepare results
 flow_list = []
 for stn_id, results in flow_dict.items():
     res1 = results.to_dataframe().reset_index()
@@ -101,7 +241,6 @@ for stn_id, results in flow_dict.items():
     flow_list.append(res1)
 
 flow1 = pd.concat(flow_list)
-
 
 gwl_stn_ids = [s['station_id'] for s in gwl_stns]
 gwl_dict = tethys1.get_bulk_results(gwl_ds['dataset_id'], gwl_stn_ids, remove_height=True)
@@ -136,28 +275,7 @@ all_stns = flow_stns.copy()
 all_stns.extend(gwl_stns)
 all_stns.extend(precip_stns)
 
-
-def stns_dict_to_gdf(stns):
-    """
-
-    """
-    stns1 = copy.deepcopy(stns)
-    geo1 = [shapely.geometry.Point(s['geometry']['coordinates']) for s in stns1]
-
-    [s.update({'min': s['stats']['min'], 'max': s['stats']['max'], 'from_date': s['stats']['from_date'], 'to_date': s['stats']['to_date']}) for s in stns1]
-    [(s.pop('stats'), s.pop('geometry'), s.pop('results_object_key'), s.pop('virtual_station')) for s in stns1]
-    [s.pop('properties') for s in stns1 if 'properties' in s]
-
-    df1 = pd.DataFrame(stns1)
-    # df1['from_date'] = pd.to_datetime(df1['from_date'])
-    # df1['to_date'] = pd.to_datetime(df1['to_date'])
-    # df1['modified_date'] = pd.to_datetime(df1['modified_date'])
-
-    stns_gpd1 = gpd.GeoDataFrame(df1, crs=4326, geometry=geo1)
-
-    return stns_gpd1
-
-
+### Export site locations
 flow_stns_gdf = stns_dict_to_gdf(flow_stns)
 gwl_stns_gdf = stns_dict_to_gdf(gwl_stns)
 precip_stns_gdf = stns_dict_to_gdf(precip_stns)
@@ -167,34 +285,11 @@ gwl_stns_gdf.to_file(os.path.join(base_dir, gwl_stns_shp))
 precip_stns_gdf.to_file(os.path.join(base_dir, precip_stns_shp))
 
 #############################################
-### Testing
+### Analysis
 
-# data = flow1[flow1.station_id == s1].drop('station_id', axis=1).rename(columns={'time': 'ds', 'streamflow': 'y'}).set_index('ds')
-# data1 = data.resample('D').mean().reset_index()
+# TODO: rework to have all sites go through weekly and monthly trend analysis
 
-# m = Prophet()
-# m.fit(data1)
-
-# future = m.make_future_dataframe(periods=365)
-
-# forecast = m.predict(future)
-# fig2 = m.plot_components(forecast)
-# # plot.plot_components_plotly(m, forecast)
-
-# s2 = 'M35/0366'
-# s2_id = [s['station_id'] for s in gwl_stns if s['ref'] == s2][0]
-# data = gwl1[gwl1.station_id == s2_id].drop('station_id', axis=1).rename(columns={'time': 'ds', 'groundwater_depth': 'y'}).set_index('ds')
-# data1 = data.resample('D').mean().interpolate('time').reset_index()
-
-# m = Prophet()
-# m.fit(data1)
-
-# future = m.make_future_dataframe(periods=30)
-
-# forecast = m.predict(future)
-# fig2 = m.plot_components(forecast)
-
-trend_dict = {i: {} for i in groups}
+# trend_dict = {i: {} for i in groups}
 trend_m_dict = {i: {} for i in groups}
 trend_w_dict = {i: {} for i in groups}
 for g, refs in groups.items():
@@ -209,70 +304,44 @@ for g, refs in groups.items():
         # chng_pt = 0.1
         if not data.empty:
             ## weekly
-            if g == 7:
-                continue
-                # data2 = data.resample('W').sum()
-                # data2 = data1.interpolate('time', limit=1).dropna()
-            else:
+            if g != 7:
                 data1 = data.resample('D').mean()
                 data2 = data1.interpolate('time', limit=120).dropna()
                 data2 = data2.resample('W').mean()
 
-            m = Prophet(changepoint_prior_scale=chng_pt, changepoint_range=0.9, interval_width=0.95)
-            m.fit(data2.reset_index())
-            future = m.make_future_dataframe(periods=1, freq='W')
-            forecast = m.predict(future)
-            trend_w_dict[g].update({ref: (m, forecast)})
-
-            ## Monthly
-            if g == 7:
+                m = Prophet(changepoint_prior_scale=chng_pt, changepoint_range=0.9, interval_width=0.95, mcmc_samples=mcmc_samples)
+                m.fit(data2.reset_index())
+                future = m.make_future_dataframe(periods=1, freq='W')
+                forecast = m.predict(future)
+                trend_w_dict[g].update({ref: (m, forecast)})
+            else:
                 data1 = data.resample('M').sum()
                 data2 = data1.interpolate('time', limit=3).dropna()
-            else:
-                data1 = data.resample('D').mean()
-                data2 = data1.interpolate('time', limit=120).dropna()
-                data2 = data2.resample('M').mean()
 
-            m = Prophet(changepoint_prior_scale=chng_pt, changepoint_range=0.9, interval_width=0.95)
-            m.fit(data2.reset_index())
-            future = m.make_future_dataframe(periods=1, freq='M')
-            forecast = m.predict(future)
-            trend_m_dict[g].update({ref: (m, forecast)})
+                m = Prophet(changepoint_prior_scale=chng_pt, changepoint_range=0.9, interval_width=0.95, mcmc_samples=mcmc_samples)
+                m.fit(data2.reset_index())
+                future = m.make_future_dataframe(periods=1, freq='M')
+                forecast = m.predict(future)
+                trend_m_dict[g].update({ref: (m, forecast)})
 
-            # Daily
-            if g == 7:
-                continue
-                # data1 = data.resample('D').sum()
-                # data2 = data1.interpolate('time', limit=120).dropna()
-            else:
-                data1 = data.resample('D').mean()
-                data2 = data1.interpolate('time', limit=120).dropna()
 
-            m = Prophet(changepoint_prior_scale=chng_pt, changepoint_range=0.9, interval_width=0.95)
-            m.fit(data2.reset_index())
-            future = m.make_future_dataframe(periods=1)
-            forecast = m.predict(future)
-            trend_dict[g].update({ref: (m, forecast)})
 
-            # m.plot(forecast)
-            # m.plot_components(forecast)
+# group = 7
 
-group = 7
-
-for f in trend_dict[group]:
-    m1, fore = trend_dict[group][f]
-    fig = plot.plot_components(m1, fore)
-    fig2 = plot.plot(m1, fore)
+# for f in trend_dict[group]:
+#     m1, fore = trend_dict[group][f]
+#     fig = plot.plot_components(m1, fore)
+#     fig2 = plot.plot(m1, fore)
 
 
 # group = 1
 
-for f in trend_m_dict[group]:
-    m1, fore = trend_m_dict[group][f]
-    fig = plot.plot_components(m1, fore)
-    fig2 = plot.plot(m1, fore)
+# for f in trend_m_dict[group]:
+#     m1, fore = trend_m_dict[group][f]
+#     fig = plot.plot_components(m1, fore)
+#     fig2 = plot.plot(m1, fore)
 
-# group = 1
+group = 1
 
 for f in trend_w_dict[group]:
     m1, fore = trend_w_dict[group][f]
@@ -283,10 +352,9 @@ for f in trend_w_dict[group]:
 ### Plots
 
 ## Yearly trends
-set_style("white")
-set_style("ticks")
-# set_context('poster')
-set_context('talk')
+# set_style("white")
+# set_style("ticks")
+# set_context('talk')
 pal1 = color_palette()
 
 x_label = x_axis_labels['yearly_trend']
@@ -307,106 +375,50 @@ data_label = 'Yearly Trend'
 
 #         plot1.savefig(os.path.join(base_dir, 'plots', f.replace('/', '_')+'_yearly_trend.png'))
 
+now1 = pd.Timestamp.now().strftime('%Y-%m-%d')
 
+plot_dir = 'plots_' + now1
+plot_path = os.path.join(base_dir, plot_dir)
 
-for g in groups:
-    y_label = y_axis_labels[g]
+if not os.path.exists(plot_path):
+    os.mkdir(plot_path)
 
-    data_list = []
-    for f in trend_dict[g]:
+data_groups = {'weekly': trend_w_dict, 'monthly': trend_m_dict}
 
-        if g == 7:
-            m1, fore = trend_m_dict[g][f]
-        else:
-            m1, fore = trend_w_dict[g][f]
+for freq, trend_dict, in data_groups.items():
+    ax1 = None
+    for g, results in trend_dict.items():
+        sites = list(results.keys())
+        if sites:
+            y_label = y_axis_labels[g]
 
-        data1 = fore[['ds', 'trend']].rename(columns={'ds': x_label, 'trend': f}).set_index(x_label).copy()
+            colors_dict = {sites[i]: pal1[i] for i in range(len(sites))}
 
-        data_list.append(data1)
+            for f in sites:
+                m1, fore = results[f]
 
-    data_df = pd.concat(data_list, axis=1)
+                color = colors_dict[f]
 
-    ax1 = data_df.plot(ylabel=y_label, color=pal1, title=data_label, figsize=(15, 10))
-    plt.tight_layout()
-    plot1 = ax1.get_figure()
+                ax1 = plot_trends(m1, fore, x_label, y_label, f, ax=ax1, color=color)
 
-    despine()
+            plot1 = ax1.get_figure()
+            plt.legend()
 
-    plot1.savefig(os.path.join(base_dir, 'plots2', 'group_{g}_yearly_trend_from_weekly_data.png'.format(g=g)))
+            plot1.savefig(os.path.join(plot_path, 'group_{g}_trend_from_{freq}_data.png'.format(g=g, freq=freq)))
+            ax1 = None
 
+            for f in sites:
+                m1, fore = results[f]
 
-for g in groups:
-    y_label = y_axis_labels[g]
+                color = colors_dict[f]
 
-    data_list = []
-    for f in trend_dict[g]:
+                ax1 = plot_seasonality(m1, fore, x_label, y_label, f, ax=ax1, color=color)
 
-        m1, fore = trend_dict[g][f]
+            plot1 = ax1.get_figure()
+            plt.legend()
 
-        data1 = fore[['ds', 'trend']].rename(columns={'ds': x_label, 'trend': f}).set_index(x_label).copy()
-
-        data_list.append(data1)
-
-    data_df = pd.concat(data_list, axis=1)
-
-    ax1 = data_df.plot(ylabel=y_label, color=pal1, title=data_label, figsize=(15, 10))
-    plt.tight_layout()
-    plot1 = ax1.get_figure()
-
-    despine()
-
-    plot1.savefig(os.path.join(base_dir, 'plots2', 'group_{g}_yearly_trend_from_daily.png'.format(g=g)))
-
-## seasonal trends
-set_style("white")
-set_style("ticks")
-# set_context('poster')
-set_context('talk')
-pal1 = color_palette()
-
-x_label = x_axis_labels['seasonality']
-data_label = 'Seasonality'
-
-# for g in groups:
-#     for f in trend_m_dict[g]:
-#         y_label = y_axis_labels[g]
-#         m1, fore = trend_m_dict[g][f]
-
-#         data1 = fore[['ds', 'trend']].rename(columns={'ds': x_label, 'trend': data_label}).set_index(x_label).copy()
-
-#         ax1 = data1.plot(ylabel=y_label, color=pal1[0], title='Site: ' + f, figsize=(15, 10))
-#         plt.tight_layout()
-#         plot1 = ax1.get_figure()
-
-#         despine()
-
-#         plot1.savefig(os.path.join(base_dir, 'plots', f.replace('/', '_')+'_seasonality.png'))
-
-for g in groups:
-    y_label = y_axis_labels[g]
-
-    data_list = []
-    for f in trend_dict[g]:
-
-        if g == 7:
-            m1, fore = trend_m_dict[g][f]
-        else:
-            m1, fore = trend_w_dict[g][f]
-
-        data1 = fore[['ds', 'yearly']].rename(columns={'ds': x_label, 'yearly': f}).set_index(x_label).copy()
-        data2 = data1.groupby(data1.index.dayofyear).mean()
-
-        data_list.append(data2)
-
-    data_df = pd.concat(data_list, axis=1)
-
-    ax1 = data_df.plot(ylabel=y_label, color=pal1, title=data_label, figsize=(15, 10))
-    plt.tight_layout()
-    plot1 = ax1.get_figure()
-
-    despine()
-
-    plot1.savefig(os.path.join(base_dir, 'plots2', 'group_{g}_seasonality.png'.format(g=g)))
+            plot1.savefig(os.path.join(plot_path, 'group_{g}_seasonality_from_{freq}_data.png'.format(g=g, freq=freq)))
+            ax1 = None
 
 
 ## TS plots
@@ -460,30 +472,6 @@ for g in groups:
         plt.close()
 
         plot1.savefig(os.path.join(base_dir, 'plots2', f.replace('/', '_')+'_ts_plot.png'))
-
-# for g in groups:
-#     y_label = y_axis_labels[g]
-
-#     data_list = []
-#     for f in trend_dict[g]:
-
-#         m1, fore = trend_dict[g][f]
-
-#         data1 = fore[['ds', 'yearly']].rename(columns={'ds': x_label, 'yearly': f}).set_index(x_label).copy()
-#         data2 = data1.groupby(data1.index.dayofyear).mean()
-
-#         data_list.append(data2)
-
-#     data_df = pd.concat(data_list, axis=1)
-
-#     ax1 = data_df.plot(ylabel=y_label, color=pal1, title=data_label, figsize=(15, 10))
-#     plt.tight_layout()
-#     plot1 = ax1.get_figure()
-
-#     despine()
-
-#     plot1.savefig(os.path.join(base_dir, 'plots', 'group_{g}_seasonality.png'.format(g=g)))
-
 
 
 
